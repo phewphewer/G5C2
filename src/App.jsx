ddf54e5aaecda2e9fa7ae6e26f3eb885a28e4f66
Josh <joshvvidal23@gmail.com>
2025-02-19 22:46:55 +0800

    First Commit - 02-19-25

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="flex justify-around py-5">
+          <div>
+            <h1 className="text-5xl text-white">Capstone 2</h1>
+            <h3 className="text-white text-center">G-5 Presents</h3>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
