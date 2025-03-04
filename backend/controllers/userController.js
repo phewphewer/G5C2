e682188aab40a604a38210e83417dd1e6dfae8e3
iamcamzy <jrgaloriodev04@gmail.com>
2025-03-04 19:16:45 +0800

    Remove admin access to password of users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,9 +3,16 @@ const mongoose = require("mongoose");
 
 // Get all users
 const getUsers = async (req, res) => {
-    const users = await User.find({}).sort({ createdAt: -1 });
+    try {
+        const users = await User.find({})
+            .select("-password")
+            .sort({ createdAt: -1 });
+        const totalUsers = await User.countDocuments({});
 
-    res.status(200).json(users);
+        res.status(200).json({ totalUsers, users });
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
 };
 
 // Get a single user
@@ -15,7 +22,7 @@ const getUser = async (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).json({ error: "No such User" });
     }
-    const user = await User.findById(id);
+    const user = await User.findById(id).select("-password");
 
     if (!user) {
         return res.status(404).json({ error: "No such User" });
