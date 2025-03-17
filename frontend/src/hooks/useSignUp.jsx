3ab4cb823b1104c0c53891be28b0d358c7505e7d
Josh <joshvvidal23@gmail.com>
2025-03-17 16:59:22 +0800

    FIXED MASSIVE 2 DAYSERROR

--- frontend/src/hooks/useSignUp.jsx
+++ frontend/src/hooks/useSignUp.jsx
@@ -4,19 +4,22 @@ import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
 export const useSignup = () => {
   const [error, setError] = useState(null);
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(False)
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(False);
   const [isLoading, setIsLoading] = useState(null);
   const { dispatch } = useAuthContext();
 
-  const signup = async (userName, email, password) => {
+  const signup = async (username, email, password) => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("/api/user/create_account", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ userName, email, password }),
-    });
+    const response = await fetch(
+      "http://localhost/4000/api/user/create_account",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, email, password }),
+      }
+    );
     const json = await response.json();
 
     if (!response.ok) {
