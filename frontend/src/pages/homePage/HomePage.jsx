76ce0cc8bd92c93346e3a2ce51c18c52123d3fb9
Camzy <jrgaloriodev04@gmail.com>
2025-03-25 13:57:37 +0800

    Hindi whitewashed pag nag fefetch

--- frontend/src/pages/homePage/HomePage.jsx
+++ frontend/src/pages/homePage/HomePage.jsx
@@ -7,23 +7,25 @@ import MainDisplay from "./Components/MainDisplay";
 import { BrowserRouter as Router } from "react-router-dom";
 
 export default function HomePage() {
-  // Manage sidebar state at the parent level
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    // Manage sidebar state at the parent level
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  return (
-    <div className="app-container relative bg-[#050E1A]  custom-scrollbar">
-      {/* Pass state and setter to Sidebar */}
-      <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
+    return (
+        <div className="app-container relative bg-[#050E1A]  custom-scrollbar">
+            {/* Pass state and setter to Sidebar */}
+            <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
 
-      {/* Pass sidebar state to Header */}
-      <div
-        className={`main-content transition-all duration-500 ease-in-out ${
-          isSidebarOpen ? "ml-[250px]" : "ml-[50px]"
-        }`}
-      >
-        <Header isOpen={isSidebarOpen} />
-        <MainDisplay />
-      </div>
-    </div>
-  );
+            {/* Pass sidebar state to Header */}
+            <div
+                className={`main-content transition-all duration-500 ease-in-out ${
+                    isSidebarOpen ? "ml-[250px]" : "ml-[50px]"
+                }`}
+            >
+                <Header isOpen={isSidebarOpen} />
+                <div className="min-w-screen min h-screen bg-[#050E1A]">
+                    <MainDisplay />
+                </div>
+            </div>
+        </div>
+    );
 }
