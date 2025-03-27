d949e4117c64a19029eb7d0db280192c871989c1
Camzy <jrgaloriodev04@gmail.com>
2025-03-27 13:47:09 +0800

    minor fixes

--- frontend/src/pages/profilePage/ProfilePage.jsx
+++ frontend/src/pages/profilePage/ProfilePage.jsx
@@ -1,86 +1,101 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import PostCard from '../../components/posting/PostCard';
-import RightSidebar from './components/RightSidebar';
-import DashboardNavBar from './components/DashboardNavBar';
-import Header from '../homePage/Components/header';
+import React from "react";
+import { useEffect, useState } from "react";
+import PostCard from "../../components/posting/PostCard";
+import RightSidebar from "./components/RightSidebar";
+import DashboardNavBar from "./components/DashboardNavBar";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 const ProfileLayout = () => {
-  // const [user, setUser] = useState([]);
-  // useEffect(() => {
-  //   const fetchUser = async () => {
-  //     const response = await fetch('http://localhost:4000/log_in/:id');
-  //     const json = await response.json();
-  //     console.log(json);
-  //     if (response.ok) {
-  //     }
-  //   };
-  //   fetchUser();
-  // }, []);
+    const { user } = useAuthContext;
+    const [posts, setPosts] = useState([]);
+    // const [user, setUser] = useState([]);
+    // useEffect(() => {
+    //   const fetchUser = async () => {
+    //     const response = await fetch('http://localhost:4000/log_in/:id');
+    //     const json = await response.json();
+    //     console.log(json);
+    //     if (response.ok) {
+    //     }
+    //   };
+    //   fetchUser();
+    // }, []);
 
-  return (
-    <div className="app-container relative bg-[#050E1A]">
-      {/* Pass state and setter to Sidebar */}
-          <DashboardNavBar/>
-      {/* Pass sidebar state to Header */}
-      <div
-        className="main-content ml-[250px] transition-all duration-100 ease-in-out"
-      >
-        {/* <Header isOpen={isSidebarOpen} /> */}
-        {/* <div className="flex flex-col md:flex-row w-full h-screen p-4 gap-4 bg-[#283D55]"> */}
-        <div className="min-h-screen h-screen bg-[#050E1A] text-white pt-5 flex gap-4 ">
-              {/* Main Content */}
-              {/* example 4 posts */}
-              <main className="space-y-5 md:w-3/4 overflow-y-auto mr-2 custom-scrollbar">
-                <PostCard />
-              </main>
-              
-              {/* Left Sidebar */}
-              <aside className="w-full md:w-1/4 bg-[#0C172E] p-4 shadow-md rounded-lg text-white overflow-y-auto custom-scrollbar">
-                <div>
-                  <div className="flex items-center justify-start">
-                    {/* Placeholder profile */}
-                    {/* Personal details placeholder */}
-                    <div style={{ lineHeight: '1rem' }}>
-                      <strong className="text-2xl block text-[#D09966]">
-                        John Doe
-                      </strong>
-                      <small className="block text-[#E4D8BE]">johndoe@gmail.com</small>
-                      <small className="block text-[#E4D8BE]">+639512793354</small>
-                    </div>
-                  </div>
-                  <div className="border-1 border-[#283D55]"></div>
-                  <div className="mt-4 w-full space-y-4">
-                    <strong className="text-1xl bg-[#283D55] block px-2 py-3 text-[#E4D8BE]">
-                      Personal Information
-                    </strong>
-                    <div className="w-full">
-                      <div className="mt-1">
-                        <small className="text-[#D09966]">Full Name</small>
-                        <div className="flex items-center text-[#E4D8BE]">
-                          <i className="fas fa-user mr-2"></i>
-                          <strong className="font-semibold">John Doe</strong>
+    return (
+        <div className="app-container relative bg-[#050E1A]">
+            {/* Pass state and setter to Sidebar */}
+            <div className="app-container relative bg-[#050E1A] custom-scrollbar">
+                <DashboardNavBar />
+            </div>
+            {/* Pass sidebar state to Header */}
+            <div className="main-content ml-[250px] transition-all duration-100 ease-in-out">
+                {/* <Header isOpen={isSidebarOpen} /> */}
+                {/* <div className="flex flex-col md:flex-row w-full h-screen p-4 gap-4 bg-[#283D55]"> */}
+                <div className="min-h-screen h-screen bg-[#050E1A] text-white pt-5 flex gap-4 ">
+                    {/* Main Content */}
+                    {/* example 4 posts */}
+                    <main className="space-y-5 md:w-3/4 overflow-y-auto mr-2 custom-scrollbar">
+                        {/* {(user.username === post.user.username)} */}
+                        <PostCard />
+                    </main>
+
+                    {/* Left Sidebar */}
+                    <aside className="w-full md:w-1/4 bg-[#0C172E] p-4 shadow-md rounded-lg text-white overflow-y-auto custom-scrollbar">
+                        <div>
+                            <div className="flex items-center justify-start">
+                                {/* Placeholder profile */}
+                                {/* Personal details placeholder */}
+                                <div style={{ lineHeight: "1rem" }}>
+                                    <strong className="text-2xl block text-[#D09966]">
+                                        *USERNAME*
+                                    </strong>
+                                </div>
+                            </div>
+                            <div className="border-1 border-[#283D55]"></div>
+                            <div className="mt-4 w-full space-y-4">
+                                <strong className="text-1xl bg-[#283D55] block px-2 py-3 text-[#E4D8BE]">
+                                    Personal Information
+                                </strong>
+                                <div className="w-full">
+                                    <div className="mt-1">
+                                        <small className="text-[#D09966]">
+                                            Email
+                                        </small>
+                                        <div className="flex items-center text-[#E4D8BE]">
+                                            <i className="fas fa-user mr-2"></i>
+                                            <strong className="font-semibold">
+                                                *SAMPLE@EMAIL.COM*
+                                            </strong>
+                                        </div>
+                                    </div>
+                                    <div className="mt-1">
+                                        <small className="text-[#D09966]">
+                                            DATE JOINED
+                                        </small>
+                                        <div className="flex items-center text-[#E4D8BE]">
+                                            <i className="fas fa-user mr-2"></i>
+                                            <strong className="font-semibold">
+                                                *SAMPLEDATE*
+                                            </strong>
+                                        </div>
+                                    </div>
+
+                                    <div className="border-1 border-[#283D55] mt-5"></div>
+
+                                    {/* <button className="mt-5 px-4 py-2 bg-[#6D513E] text-[#E4D8BE] rounded-lg w-full hover:bg-[#D09966]">
+                                        <i className="fas fa-edit mr-5"></i>
+                                        EDIT PROFILE
+                                    </button> */}
+                                </div>
+                            </div>
                         </div>
-                      </div>
+                    </aside>
 
-                      <div className="border-1 border-[#283D55] mt-5"></div>
-
-                      <button className="mt-5 px-4 py-2 bg-[#6D513E] text-[#E4D8BE] rounded-lg w-full hover:bg-[#D09966]">
-                        <i className="fas fa-edit mr-5"></i>
-                        EDIT PROFILE
-                      </button>
-                    </div>
-                  </div>
-                  <LeftSidebar />
+                    {/* Remove Right Sidebar */}
+                    {/* <RightSidebar /> */}
                 </div>
-              </aside>
-
-              {/* Remove Right Sidebar */}
-              {/* <RightSidebar /> */}
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProfileLayout;
--- frontend/src/pages/profilePage/components/DashboardNavBar.jsx
+++ frontend/src/pages/profilePage/components/DashboardNavBar.jsx
@@ -3,120 +3,125 @@ import { useRef, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Sidebar({ isOpen, setIsOpen }) {
-  const [isAboutOpen, setIsAboutOpen] = useState(false);
-  const [isContactInfoOpen, setIsContactInfoOpen] = useState(false);
-  const [isSupportUsOpen, setIsSupportUsOpen] = useState(false);
-  const dropdownRef = useRef(null);
+    const [isAboutOpen, setIsAboutOpen] = useState(false);
+    const [isContactInfoOpen, setIsContactInfoOpen] = useState(false);
+    const [isSupportUsOpen, setIsSupportUsOpen] = useState(false);
+    const dropdownRef = useRef(null);
 
-  const toggleAbout = () => {
-    setIsAboutOpen((prev) => !prev);
-  };
-  const toggleContact = () => {
-    setIsContactInfoOpen((prev) => !prev);
-  };
-  const toggleSupport = () => {
-    setIsSupportUsOpen((prev) => !prev);
-  };
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setIsAboutOpen(false);
-        setIsContactInfoOpen(false);
-        setIsSupportUsOpen(false);
-      }
+    const toggleAbout = () => {
+        setIsAboutOpen((prev) => !prev);
     };
-    // Only add the event listener if the sidebar is open
-    if (isOpen) {
-      document.addEventListener("mousedown", handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+    const toggleContact = () => {
+        setIsContactInfoOpen((prev) => !prev);
     };
-  }, [isOpen]);
+    const toggleSupport = () => {
+        setIsSupportUsOpen((prev) => !prev);
+    };
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                dropdownRef.current &&
+                !dropdownRef.current.contains(event.target)
+            ) {
+                setIsAboutOpen(false);
+                setIsContactInfoOpen(false);
+                setIsSupportUsOpen(false);
+            }
+        };
+        // Only add the event listener if the sidebar is open
+        if (isOpen) {
+            document.addEventListener("mousedown", handleClickOutside);
+        }
 
-  return (
-    <>
-      {/* Sidebar */}
-      <div
-        className="fixed top-0 left-0 w-[230px] h-screen bg-[#0A1A2F] border-r-[1px] border-[#172A42] text-[#F7FAFC]
-                   transition-transform duration-100 ease-in-out z-[1000]"
-        ref={dropdownRef}
-      >
-        <ul className="list-none p-0 mt-[50px]">
-          <li className="py-[15px] px-[20px] text-[18px] cursor-pointer text-[#CBD5E1] hover:bg-white/10 hover:rounded-md hover:text-[#F7FAFC]">
-            <Link to="/home">
-              <button className="w-full h-full hover:cursor-pointer">
-                Home
-              </button>
-            </Link>
-          </li>
-          <li className=" py-[15px] px-[20px] text-[18px] cursor-pointer text-[#CBD5E1] hover:bg-white/10 hover:rounded-md hover:text-[#F7FAFC]">
-            <button
-              className="w-full h-full hover:cursor-pointer "
-              onClick={toggleAbout}
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isOpen]);
+
+    return (
+        <>
+            {/* Sidebar */}
+            <div
+                className="fixed top-0 left-0 w-[230px] h-screen bg-[#0A1A2F] border-r-[1px] border-[#172A42] text-[#F7FAFC]
+                   transition-transform duration-500 ease-in-out z-[1000]"
+                ref={dropdownRef}
             >
-              About
-            </button>
-            {isAboutOpen && (
-              <div className="mt-2 p-4 bg-[#1A2B44] rounded-md text-[#F7FAFC] overflow-y-auto max-h-[100px] custom-scrollbar">
-                <p className=" text-sm font-semibold italic">
-                  Droplet is an open forum for all accepting all valid opinions
-                  and topics, a hub for communication, a stage to ripple
-                  discussions
-                </p>
-              </div>
-            )}
-          </li>
-          <li className="py-[15px] px-[20px] text-[18px] cursor-pointer text-[#CBD5E1] hover:bg-white/10 hover:rounded-md hover:text-[#F7FAFC]">
-            <button
-              className="w-full h-full hover:cursor-pointer"
-              onClick={toggleContact}
-            >
-              Contact Us
-            </button>
-            {isContactInfoOpen && (
-              <div className="mt-2 p-4 bg-[#1A2B44] rounded-md text-[#F7FAFC] overflow-y-auto max-h-[100px] custom-scrollbar">
-                <p className=" text-sm">drizzleCo@gmail.com</p>
-              </div>
-            )}
-          </li>
-          <li className="py-[15px] px-[20px] text-[18px] cursor-pointer text-[#CBD5E1] hover:bg-white/10 hover:rounded-md hover:text-[#F7FAFC]">
-            <button
-              className="w-full h-full hover:cursor-pointer"
-              onClick={toggleSupport}
-            >
-              Support Us
-            </button>
-            {isSupportUsOpen && (
-              <div className="mt-2 p-4 bg-[#1A2B44] rounded-md text-[#F7FAFC] overflow-y-auto max-h-[100px] custom-scrollbar">
-                <div className="flex text-[0.80rem] justify-around">
-                  <a
-                    href="https://ko-fi.com"
-                    target="_blank"
-                    className="font-extrabold"
-                  >
-                    BUY ME A{" "}
-                    <span className="text-amber-800 hover:text-amber-700">
-                      COFFEE
-                    </span>
-                  </a>
+                <ul className="list-none p-0 mt-[50px]">
+                    <li className="py-[15px] px-[20px] text-[18px] cursor-pointer text-[#CBD5E1] hover:bg-white/10 hover:rounded-md hover:text-[#F7FAFC]">
+                        <Link to="/home">
+                            <button className="w-full h-full hover:cursor-pointer">
+                                Home
+                            </button>
+                        </Link>
+                    </li>
+                    <li className="cursor-pointer text-[#CBD5E1] hover:bg-white/10 hover:rounded-md hover:text-[#F7FAFC]">
+                        <button
+                            className=" py-[15px] px-[20px] text-[18px] w-full h-full hover:cursor-pointer "
+                            onClick={toggleAbout}
+                        >
+                            About
+                        </button>
+                        {isAboutOpen && (
+                            <div className="mt-2 p-4 bg-[#1A2B44] rounded-md text-[#F7FAFC] overflow-y-auto max-h-[100px] custom-scrollbar">
+                                <p className=" text-sm font-semibold italic text-justify">
+                                    Droplet is an open forum for all accepting
+                                    all valid opinions and topics, a hub for
+                                    communication, a stage to ripple discussions
+                                </p>
+                            </div>
+                        )}
+                    </li>
+                    <li className="cursor-pointer text-[#CBD5E1] hover:bg-white/10 hover:rounded-md hover:text-[#F7FAFC]">
+                        <button
+                            className="py-[15px] px-[20px] text-[18px] w-full h-full hover:cursor-pointer"
+                            onClick={toggleContact}
+                        >
+                            Contact Us
+                        </button>
+                        {isContactInfoOpen && (
+                            <div className="mt-2 p-4 bg-[#1A2B44] rounded-md text-[#F7FAFC] overflow-y-auto max-h-[100px] custom-scrollbar">
+                                <p className=" text-sm">drizzleCo@gmail.com</p>
+                            </div>
+                        )}
+                    </li>
+                    <li className="cursor-pointer text-[#CBD5E1] hover:bg-white/10 hover:rounded-md hover:text-[#F7FAFC]">
+                        <button
+                            className="py-[15px] px-[20px] text-[18px] w-full h-full hover:cursor-pointer"
+                            onClick={toggleSupport}
+                        >
+                            Support Us
+                        </button>
+                        {isSupportUsOpen && (
+                            <div className="mt-2 p-4 bg-[#1A2B44] rounded-md text-[#F7FAFC] overflow-y-auto max-h-[100px] custom-scrollbar">
+                                <div className="flex text-[0.80rem] justify-around">
+                                    <a
+                                        href="https://ko-fi.com"
+                                        target="_blank"
+                                        className="font-extrabold"
+                                    >
+                                        BUY ME A{" "}
+                                        <span className="text-amber-800 hover:text-amber-700">
+                                            COFFEE
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
+                        )}
+                    </li>
+                </ul>
+                <div className="w-full flex justify-center items-center absolute bottom-0">
+                    <div className="text-sm flex justify-around text-gray-500">
+                        <Link to="../feedback_form">
+                            <button>
+                                give us your feedback{" "}
+                                <span className="text-blue-700 hover:text-blue-400">
+                                    here
+                                </span>
+                            </button>
+                        </Link>
+                    </div>
                 </div>
-              </div>
-            )}
-          </li>
-        </ul>
-        <div className="w-full flexbox flex-wrap fixed bottom-2 left-0">
-          <div className="text-sm flex justify-around text-gray-500">
-            <Link to="../feedback_form">
-              <button>
-                give us your feedback{" "}
-                <span className="text-blue-700 hover:text-blue-400">here</span>
-              </button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+            </div>
+        </>
+    );
 }
